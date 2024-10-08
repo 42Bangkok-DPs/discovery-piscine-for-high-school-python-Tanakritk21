25cdfa7da4be86139722492c3af85c6100f82e0f
Tanakritk21 <tanakritk21@gmail.com>
2024-10-08 21:04:50 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,2 +1,5 @@
-your_age = int(input("Please tell me your age "))
-print (f"You are currently {your_age} years old. \n In 10 years, you'll be {your_age +10} years old. \n In 20 years, you'll be {your_age + 20} years old. \n In 30 years, you'll be {your_age + 30} years old.")
+your_age = int(input("Please tell me your age : "))
+print (f"You are currently {your_age} years old." )
+print(f"In 10 years, you'll be {your_age +10} years old. ")
+print(f"In 20 years, you'll be {your_age + 20} years old. ")
+print(f"In 30 years, you'll be {your_age + 30} years old.")
