cb12506b6084df4e99473103b1ada1d78ed6faf8
Tanakritk21 <tanakritk21@gmail.com>
2024-10-08 12:25:54 +0700

    Create mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+first_number = int(input("Enter your first number :"),end="\n")
+second_number = int(input("Enter your second number :"),\n)
+
+ print(first_number, " x ", second_number, " = " ,first_number*second_number)
+
+if first_number*second_number < 0:
+  print("This number is negative.")
+elif first_number*second_number  > 0:
+  print("This number is positive.")
+else:
+  print("This number is both positive and negative.")
