13cf729ef467735975967ad2b7468a34519338cb
Tanakritk21 <tanakritk21@gmail.com>
2024-10-10 10:09:28 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 New_array =[ ]
 
 for i in range(len(Original_array)):
     if Original_array[i] > 5:
-        New_array.append(Original_array[i] + 2)
+        New_array += [Original_array[i] + 2]
 
 print(Original_array,"\n", New_array)
